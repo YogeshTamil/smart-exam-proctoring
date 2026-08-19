c80374d6b590be3623c76a304cb5802774537454
Yogesh <22112171@student.hindustanuniv.ac.in>
2026-08-19 09:55:36 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -243,5 +243,8 @@ def register_routes(app):
 
 app = create_app()
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
